6188c9e9da62efed5a775db890c8c0f8b3d78164
Amreesh Tyagi <tyagi.amreesh@gmail.com>
2018-09-07 08:09:23 +0530

    adding test endpoint

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.get('/hello/:name', function(req, res) {
   res.send('Hello, ' + req.params.name + ' Welcome to demo app! at '+os.hostname());
 });
 
+app.get('/test', function (req, res) {
+  res.send({
+    req: { headers: req.headers, body: req.body, params: req.params, path: req.path }
+    , res: { headers: res.headers, body: res.body, params: res.params, path: res.path}
+  });
+});
+
 app.listen(5000, function () {
   console.log('Server is running  at http://localhost:5000');
 });
